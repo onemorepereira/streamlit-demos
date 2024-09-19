95c9f0f11c3448100ab27aca9ec5ace794f2358e
Miguel Pereira <miguel@myrotarycar.com>
2024-09-19 08:12:10 -0500

    Replace wheel size drop-down Replace speed(dia) --> float() Set funky page_icon Add cassette / chainring to speed chart

--- cycling-gearing-calculator/main.py
+++ cycling-gearing-calculator/main.py
@@ -2,15 +2,18 @@ import data
 import streamlit as st
 import pandas as pd
 import math
+import altair as alt
 
 # This definitely could use some work...
-def speed(gr: float, rpm: int = 100, dia: int = 29) -> float:
-    return round((rpm * gr * (dia+1) * math.pi) / 60 / 19.55, 1)
+def speed(gr: float, rpm: int = 100, dia: float = 29) -> float:
+    return round((rpm * gr * dia * math.pi) / 60 / 19.55, 1)
 
 
 st.set_page_config(
     page_title="Cycling Gear Calculator",
-    layout="wide",
+    layout="centered",
+    page_icon="⚙️",
+    initial_sidebar_state="collapsed"
 )
 
 st.title('Cycling Gear Calculator')
@@ -18,12 +21,11 @@ st.title('Cycling Gear Calculator')
 CHAINRINGS = data.CHAINRINGS
 CASSETTES  = data.CASSETTES
 CRANKARMS  = data.CRANKARMS
-WHEELS     = data.WHEELS
 
 cassette  = st.selectbox("Select your cassette",       CASSETTES)
 chainring = st.selectbox("Select your chainring",      CHAINRINGS)
 crankarms = st.selectbox("Select your crank length",   CRANKARMS, index=3)
-wheels    = st.selectbox("Select your wheel diameter", WHEELS,  index=3)
+wheels    = st.text_input("Enter you total wheel + tire diameter (inches)", value=29)
 
 cas_details = CASSETTES[cassette]
 cha_details = CHAINRINGS[chainring]
@@ -59,10 +61,10 @@ st.slider("Gearing Range",
           )
 
 # Crankarm Display
-st.write("Crankarm selected",  crankarms)
+st.write("Crankarm selected",  crankarms, "cm")
 
 # Wheel Diameter Display
-st.write("Wheel diamater",  wheels)
+st.write("Wheel diamater",  wheels, "inches")
 
 # Concatenate the pivoted DF's
 frames = [ 
@@ -85,6 +87,12 @@ st.title("Gear Ratios")
 st.dataframe(matrix.reset_index(), height=35*len(matrix)+38)
 
 st.title("Speed")
+# Quick slider to select target pedaling RPM
 rpm = st.slider("Pedaling RPM", value = 90, min_value=30, max_value=120, step=5)
-w = matrix.apply(speed, args=(rpm, wheels))
+w   = matrix.apply(speed, args=(float(rpm), float(wheels)))
+
+# Display theoretical max speed on each gear
 st.dataframe(w, height=35*len(matrix)+38)
+
+# Chart max speed on each gear
+st.line_chart(data=w, x_label="Cassette Teeth", y_label="Speed (mph)")
